7aa4bde9c940570ce721f3c4f1c63dc6e1a93769
Vibha3010 <47321745+Vibha3010@users.noreply.github.com>
2019-02-04 21:08:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
+// var target= require('vibhavashistha');
+// target.start(function(o){
+//     var s = o.new('nodeExample'); //instantiate StringBuffer. 
+//     s.displayInformation();  // append hello       // dlrow ollew
+//     console.log('->',s); // [function body]
+//   })
+
+
 const exec= require('child_process').exec;
-const childProcess= exec('java -jar C:\\Users\\tekiz\\workspace\\Products\\branches\\secure-trace\\r1\\modules\\securetrace-analyser\\target\\securetrace-analyser-1.2.5.jar "C:\\Users\\tekiz\\Downloads\\jason.docx C:\\secure"',function(err,stdout,stderr) {
+const childProcess= exec('java -jar C:\\Users\\tekiz\\nodeHttp\\securetrace-analyser-1.2.5.jar "C:\\Users\\tekiz\\nodeHttp\\Jason_Engle.pdf C:\\Users\\tekiz\\nodeHttp"',function(err,stdout,stderr) {
 if(err) {
     console.log(err)
 }
